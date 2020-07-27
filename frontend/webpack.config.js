800afb66b86e1491b9333efe8ed78107de69835d
thallysfs <50297870+thallysfs@users.noreply.github.com>
2020-07-27 15:02:11 -0300

    ajuste webpackconfig

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -14,7 +14,6 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            //apelido, sempre que eu me refirir ao 'node_modules', basta usar 'modules'
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
@@ -34,16 +33,16 @@ module.exports = {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                preset: ['es2015', 'react'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
         }, {
-            teste: /\.css/,
+            test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            teste: /\.woff|.woff2|.ttf|.oet|.svg|.png|.jpg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
-        }]
+        }
+        ]
     }
 }
-
